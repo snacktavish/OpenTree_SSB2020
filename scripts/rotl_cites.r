843df5318bb6e521fe0fbb83ac8f32f03d60d0bb
Luna Luisa Sanchez Reyes <sanchez.reyes.luna@gmail.com>
2019-12-06 11:32:51 -0800

    working now!
    but (accidentally) made pull request to ropensci/rotl and did not pass the checks there

--- scripts/rotl_cites.r
+++ scripts/rotl_cites.r
@@ -1,6 +1,8 @@
 library(devtools)
 install_github("McTavishLab/rotl")
+# you can also install the dev versio of the package from a branch with:
+# devtools::install_github("McTavishLab/rotl", ref = "citations_doc")
 library(rotl)
 
-tol_induced_subtree(c(292466, 267845, 316878, 102710), get_citations = TRUE)
-tol_induced_subtree(c(292466, 267845, 316878, 102710), file = "test", get_citations = TRUE)
+tol_induced_subtree(c(292466, 267845, 316878, 102710), write_citations = TRUE)
+tol_induced_subtree(c(292466, 267845, 316878, 102710), file = "test", write_citations = TRUE)
